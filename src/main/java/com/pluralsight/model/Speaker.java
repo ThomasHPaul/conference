eb93224d948ccc954e70207d711c5f1d8cd2cfed
Thomas Paul <thomaspauljs@outlook.com>
2023-05-31 06:32:45 -0500

    Init commit; following Spring 5 fundamentals Pluralsight course

---
+++ src/main/java/com/pluralsight/model/Speaker.java
@@ -0,0 +1,14 @@
+package com.pluralsight.model;
+
+public class Speaker {
+
+    private String firstName;
+    private String lastName;
+
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFirstName() { return this.firstName; }
+    public String getLastName() { return this.lastName; }
+
+
+}
